4b2e4f11d00896922cc308717faa93c5bd3982e2
JinpingMa <hyperbola615@gmail.com>
2016-08-05 09:20:31 +0800

    add locations-list and location-info page

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,11 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var ctrlLocations = require('../controllers/locations');
+var ctrlOthers = require('../controllers/others');
 
-/* GET home page. */
+/* Locations pages */
+router.get('/', ctrlLocations.homelist);
+router.get('/location', ctrlLocations.locationInfo);
+router.get('/location/review/new', ctrlLocations.addReview);
+
+/* Other pages */
+router.get('/about', ctrlOthers.about);
+
+/* GET home page. 
 router.get('/', function (req, res, next) {
 	res.render('index', {
 		title: 'Mypage'
 	});
-});
+});*/
 
 module.exports = router;
---
+++ readme.md
@@ -0,0 +1,1 @@
+Looking for wifi and a seat? Loc8r helps you find places to work when out and about. Perhaps with coffee, cake or a pint? Let Loc8r help you find the place you're looking for.
