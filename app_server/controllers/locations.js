4a54fdb4ab1cb20c3ac683f60b7b6c8d19748c41
JinpingMa <hyperbola615@gmail.com>
2016-08-05 23:45:54 +0800

    move data from vies to the cotroller

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,20 +1,89 @@
 /* GET 'home' page */
 module.exports.homelist = function (req, res) {
-    res.render('locations-list', {
-        title: 'Home'
+    res.render('locations-list', { 
+      title: 'WiFiDot - find a place to work with wifi',
+    	pageHeader: {
+    	  title: 'WiFiDot',
+    	  strapline: 'Find place to work with wifi near you!'
+      },
+      sidebar: "Looking for wifi and a seat? WiFiDot helps you find places to work when out and about. Perhaps with coffee, cake or a pint? Let WiFiDot help you find the place you're looking for.",
+      locations:[{
+        name: 'Starcups',
+        address: '125 High Street, Reading, RG6 1PS',
+        rating: 3,
+        facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+        distance: '100m'
+      },{
+        name: 'Cafe Hero',
+        address: '125 High Street, Reading, RG6 1PS',
+        rating: 4,
+        facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+        distance: '200m'
+      },{
+        name: 'Burger Queen',
+        address: '125 High Street, Reading, RG6 1PS',
+        rating: 2,
+        facilities: ['Food', 'Premium wifi'],
+        distance: '250m'
+      }]
     });
 };
 
 /*GET 'Location info' page */
 module.exports.locationInfo = function (req, res) {
     res.render('location-info', {
-        title: 'Location info'
+        title: 'Starcups',
+        pageHeader: {
+          title: 'Starcups'
+        },
+        sidebar: {
+          context: 'is on WiFiDot because it has accessible wifi and spance to sit down with your laptop and get some work done.',
+          callToAction: 'if you\'ve been and you like it - or if you don\'t - please leave a review to help other people just like you.'
+        },
+        location: {
+          name: 'Starcups',
+          address: '125 High Street, Reading, RG6 1PS',
+          rating: 3,
+          facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+          coords: {
+            lat: 51.455041,
+            lng: -0.9690884
+          },
+          openingTimes: [{
+            days: 'Monday - Friday',
+            opening: '7:00am',
+            closing: '7:00pm',
+            closed: false
+          },{
+            days: 'Saturday',
+            opening: '8:00am',
+            closing: '5:00pm',
+            closed: false
+          },{
+            days: 'sunday',
+            closed: true
+          }],
+          reviews: [{
+            author: 'Hyperbola',
+            rating: 5,
+            timestamp: '16 July 2015',
+            reviewText: 'What a great place. I can\'t say enough good things about it.'
+          },{
+            author: 'Lu Sai',
+            rating: 3,
+            timestamp: '16 June 2015',
+            reviewText: 'It was okay. Coffee wasn\'t great, but the wifi was fast.'
+          }]
+        }
     });
 };
 
 /* GET 'Add review' page */
 module.exports.addReview = function (req, res) {
     res.render('location-review-form', {
-        title: 'Add review'
+        title: 'Review Starcups on WiFiDot',
+        pageHeader: {
+          title: 'Review Starcups'
+        }
     });
 };
--- app_server/controllers/others.js
+++ app_server/controllers/others.js
@@ -1,6 +1,7 @@
 /* GET home page. */
 module.exports.about = function (req, res) {
 	res.render('generic-text', {
-		title: 'About'
+		title: 'About WiFiDot',
+		content: 'WiFiDot was created to help people find places to sit down and get a bit of work done.\n\nWiFiDot 是一个帮助人们找到一个有wifi的地方，在这个地方人们可以完成一些紧急的工作。'
 	});
 };
