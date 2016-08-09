1898dbeccab60e581b7bb60937c08d04e741a66f
JinpingMa <hyperbola615@gmail.com>
2016-08-09 11:50:52 +0800

    views connects to db

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -77,53 +77,57 @@ module.exports.homelist = function(req, res) {
     });
 };
 
-/*GET 'Location info' page */
-module.exports.locationInfo = function (req, res) {
-    res.render('location-info', {
-        title: 'Starcups',
-        pageHeader: {
-          title: 'Starcups'
+var renderDetailPage = function (req, res) {
+  res.render('location-info', {
+      title: 'Starcups',
+      pageHeader: {
+        title: 'Starcups'
+      },
+      sidebar: {
+        context: 'is on WiFiDot because it has accessible wifi and spance to sit down with your laptop and get some work done.',
+        callToAction: 'if you\'ve been and you like it - or if you don\'t - please leave a review to help other people just like you.'
+      },
+      location: {
+        name: 'Starcups',
+        address: '125 High Street, Reading, RG6 1PS',
+        rating: 3,
+        facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+        coords: {
+          lat: 51.455041,
+          lng: -0.9690884
         },
-        sidebar: {
-          context: 'is on WiFiDot because it has accessible wifi and spance to sit down with your laptop and get some work done.',
-          callToAction: 'if you\'ve been and you like it - or if you don\'t - please leave a review to help other people just like you.'
-        },
-        location: {
-          name: 'Starcups',
-          address: '125 High Street, Reading, RG6 1PS',
+        openingTimes: [{
+          days: 'Monday - Friday',
+          opening: '7:00am',
+          closing: '7:00pm',
+          closed: false
+        },{
+          days: 'Saturday',
+          opening: '8:00am',
+          closing: '5:00pm',
+          closed: false
+        },{
+          days: 'sunday',
+          closed: true
+        }],
+        reviews: [{
+          author: 'Hyperbola',
+          rating: 5,
+          timestamp: '16 July 2015',
+          reviewText: 'What a great place. I can\'t say enough good things about it.'
+        },{
+          author: 'Lu Sai',
           rating: 3,
-          facilities: ['Hot drinks', 'Food', 'Premium wifi'],
-          coords: {
-            lat: 51.455041,
-            lng: -0.9690884
-          },
-          openingTimes: [{
-            days: 'Monday - Friday',
-            opening: '7:00am',
-            closing: '7:00pm',
-            closed: false
-          },{
-            days: 'Saturday',
-            opening: '8:00am',
-            closing: '5:00pm',
-            closed: false
-          },{
-            days: 'sunday',
-            closed: true
-          }],
-          reviews: [{
-            author: 'Hyperbola',
-            rating: 5,
-            timestamp: '16 July 2015',
-            reviewText: 'What a great place. I can\'t say enough good things about it.'
-          },{
-            author: 'Lu Sai',
-            rating: 3,
-            timestamp: '16 June 2015',
-            reviewText: 'It was okay. Coffee wasn\'t great, but the wifi was fast.'
-          }]
-        }
-    });
+          timestamp: '16 June 2015',
+          reviewText: 'It was okay. Coffee wasn\'t great, but the wifi was fast.'
+        }]
+      }
+  });
+};
+
+/*GET 'Location info' page */
+module.exports.locationInfo = function(req, res) {
+  renderDetailPage(req, res);
 };
 
 /* GET 'Add review' page */
