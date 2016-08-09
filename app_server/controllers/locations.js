426030c744ee0769be993ccb171bf760d62eaba6
JinpingMa <hyperbola615@gmail.com>
2016-08-09 17:28:02 +0800

    views connects to db

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -107,8 +107,7 @@ var renderDetailPage = function (req, res, locDetail) {
   });
 };
 
-/*GET 'Location info' page */
-module.exports.locationInfo = function(req, res) {
+var getLocationInfo = function(req, res, callback) {
   var requestOptions, path;
   path = "/api/locations/" + req.params.locationid;
   requestOptions = {
@@ -125,19 +124,53 @@ module.exports.locationInfo = function(req, res) {
           lng: body.coords[0],
           lat: body.coords[1]
         };
-        renderDetailPage(req, res, data);
+        callback(req, res, data);
       } else {
         _showError(req, res, response.statusCode);
       }   
   });
 };
+/*GET 'Location info' page */
+module.exports.locationInfo = function(req, res) {
+  getLocationInfo(req, res, function(req, res, responseData) {
+    renderDetailPage(req, res, responseData);
+  });
+};
 
-/* GET 'Add review' page */
-module.exports.addReview = function (req, res) {
+var renderReviewForm = function (req, res, locDetail) {
     res.render('location-review-form', {
-        title: 'Review Starcups on WiFiDot',
-        pageHeader: {
-          title: 'Review Starcups'
-        }
+        title: 'Review ' + locDetail.name + ' on WiFiDot',
+        pageHeader: { title: 'Review ' + locDetail.name }
+        //error: req.query.err
+    });
+};
+/* GET 'Add review' page */
+module.exports.addReview = function(req, res){
+  getLocationInfo(req, res, function(req, res, responseData) {
+    renderReviewForm(req, res, responseData);
+  });
+};
+module.exports.doAddReview = function(req, res) {
+  var requestOptions, path, locationid, postdata;
+  locationid = req.params.locationid;
+  path = "/api/locations/" + locationid + "/reviews";
+  postdata = {
+    author: req.body.name,
+    rating: parseInt(req.body.rating, 10),
+    reviewText: req.body.review
+  };
+  requestOptions = {
+    url : apiOptions.server + path,
+    method : "POST",
+    json : postdata
+  };
+  request (
+    requestOptions,
+    function(err, response, body) {
+      if (responseData.statusCode === 201) {
+        res.redirect('/location/' + locationid);
+      } else {
+        _showError(req, res, response.statusCode);
+      }
     });
 };
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -7,7 +7,7 @@ var ctrlOthers = require('../controllers/others');
 router.get('/', ctrlLocations.homelist);
 router.get('/location/:locationid', ctrlLocations.locationInfo);
 router.get('/location/:locationid/review/new', ctrlLocations.addReview);
-//router.post('/location/:locationid/review/new',ctrlLocations.doAddReview);
+router.post('/location/:locationid/review/new',ctrlLocations.doAddReview);
 
 /* Other pages */
 router.get('/about', ctrlOthers.about);
