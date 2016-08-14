d20c08d752d770c2635381c2762687a6e7a7143b
JinpingMa <hyperbola615@gmail.com>
2016-08-14 11:38:13 +0800

    #

--- app.js
+++ app.js
@@ -29,9 +29,9 @@ var appClientFiles = [
   'app_client/common/directives/footerGeneric/footerGeneric.directive.js',
   'app_client/common/directives/navigation/navigation.directive.js',
   'app_client/common/directives/pageHeader/pageHeader.directive.js'
-  ];
+];
 var uglified = uglifyJs.minify(appClientFiles, { compress : false });
-//console.log(uglified.code);
+console.log(uglified.code);
 
 fs.writeFile('public/angular/wifidot.min.js', uglified.code, function (err) {
   if(err) {
