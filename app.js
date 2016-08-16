49093f3db9c68de50c6749bfe43756cccfeb0444
JinpingMa <hyperbola615@gmail.com>
2016-08-17 00:00:04 +0800

    add register and login api

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
+require('dotenv').load();
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var passport = require('passport');
+
 require('./app_api/models/db');
+require('./app_api/config/passport');
 var uglifyJs = require("uglify-js");
 var fs = require('fs');
 
@@ -55,6 +59,8 @@ app.use(bodyParser.urlencoded({
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'app_client')));
+
+app.use(passport.initialize());
 
 //app.use('/', routes);
 app.use('/api', routesApi);
--- app_api/models/db.js
+++ app_api/models/db.js
@@ -43,4 +43,5 @@ process.on('SIGINT', function(){
 });
 
 require('./locations');
+require('./users');
 
