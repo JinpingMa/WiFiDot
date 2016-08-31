8a03a0589dd6f3c926fa2fd61c5c6c4a7b7415c4
JinpingMa <hyperbola615@gmail.com>
2016-08-31 16:42:11 +0800

    fixed mapbug,use baidustaticmap

--- app_client/locationDetail/locationDetail.controller.js
+++ app_client/locationDetail/locationDetail.controller.js
@@ -27,26 +27,26 @@
         console.log(e);
       });
 
-      vm.popupReviewForm = function () {
-        //alert("Let's add a review!");
-        var modalInstance = $modal.open({
-          templateUrl: '/reviewModal/reviewModal.view.html',
-          controller: 'reviewModalCtrl as vm',
-          resolve: {
-            locationData : function () {
-              return {
-                locationid : vm.locationid,
-                locationName : vm.data.location.name
-              };
-            }
+    vm.popupReviewForm = function () {
+      //alert("Let's add a review!");
+      var modalInstance = $modal.open({
+        templateUrl: '/reviewModal/reviewModal.view.html',
+        controller: 'reviewModalCtrl as vm',
+        resolve: {
+          locationData : function () {
+            return {
+              locationid : vm.locationid,
+              locationName : vm.data.location.name
+            };
           }
-        });
-      
-        modalInstance.result.then(function (data) {
-          vm.data.location.reviews.push(data);
-        });
+        }
+      });
+    
+      modalInstance.result.then(function (data) {
+        vm.data.location.reviews.push(data);
+      });
 
-      };
+    };
     
   }
 })();
