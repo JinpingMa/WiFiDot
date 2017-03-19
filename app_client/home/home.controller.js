db8260b3cfd6dc1dd31b2ba3f4c45bfee3df0184
JinpingMa <hyperbola615@gmail.com>
2017-03-19 19:37:33 +0800

    使用百度地图

--- app_client/home/home.controller.js
+++ app_client/home/home.controller.js
@@ -20,7 +20,38 @@
     };
     vm.message = "Checking your location";
 
+    //get location by baidu
     vm.getData = function (position) {
+      if (this.getStatus() == BMAP_STATUS_SUCCESS) {
+        var lat = position.point.lat;
+        var lng = position.point.lng;
+        //console.log("定位的位置" + lat +"\n" + lng);
+        //alert('your location' + lat +', ' + lng);
+        vm.message = "Search for nearby places";
+        wifidotData.locationByCoords(lat, lng)
+          .success(function(data) {
+            vm.message = data.length > 0 ? "" : "No locations found nearby";
+            vm.data = { locations : data };
+          })
+          .error(function (e) {
+            vm.message = "Sorry,something's gone wrong, please try again later";
+          });
+      } else {
+        //alert('failed' + this.getStatus());
+        vm.message= 'failed' + this.getStatus();
+      }
+    };
+    vm.noGeo = function () {
+      $scope.$apply(function() {
+        vm.message = "Geolocation not supported by this brower.";
+      });
+    };
+    geolocation.getPosition(vm.getData,vm.noGeo);
+
+
+
+
+    /*vm.getData = function (position) {
       var lat = position.coords.latitude,
           lng = position.coords.longitude;
       vm.message = "Search for nearby places";
@@ -46,7 +77,7 @@
       });
     };
 
-    geolocation.getPosition(vm.getData,vm.showError,vm.noGeo);
+    geolocation.getPosition(vm.getData,vm.showError,vm.noGeo);*/
 
   }
 
